4dc0301631744fe1a1264daa2752821c3067a856
andrewshan <andrewshan@tencent.com>
2021-09-11 21:59:49 +0800

    submit build scripts

--- build.sh
+++ build.sh
@@ -4,10 +4,15 @@ set -e
 
 workdir=$(dirname $(realpath $0))
 version=$(cat version 2>/dev/null)
+bin_name="polaris-server"
 GOOS=`go env GOOS`
 GOARCH=`go env GOARCH`
 folder_name="polaris-server-release_${version}.${GOOS}.${GOARCH}"
 pkg_name="${folder_name}.zip"
+if [ ${GOOS} -eq "windows" ];then
+  bin_name="polaris-server.exe"
+fi
+echo "binary name is ${bin_name}"
 
 cd $workdir
 
@@ -16,15 +21,15 @@ rm -rf ${folder_name}
 rm -f "${pkg_name}"
 
 # 编译
-rm -f polaris-server
+rm -f ${bin_name}
 
 build_date=$(date "+%Y%m%d.%H%M%S")
 package="github.com/polarismesh/polaris-server/common/version"
-go build -ldflags="-X ${package}.Version=${version} -X ${package}.BuildDate=${build_date}"
+go build -o ${bin_name} -ldflags="-X ${package}.Version=${version} -X ${package}.BuildDate=${build_date}"
 
 # 打包
 mkdir -p ${folder_name}
-mv polaris-server ${folder_name}
+mv ${bin_name} ${folder_name}
 cp polaris-server.yaml ${folder_name}
 cp -r tool ${folder_name}/
 zip -r "${pkg_name}" ${folder_name}
