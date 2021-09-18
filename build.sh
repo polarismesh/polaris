974136bac05b2e63a6b5a1682f4c7f64a9a32177
yukang <moorekang@gmail.com>
2021-09-18 14:38:36 +0000

    fix script error

--- build.sh
+++ build.sh
@@ -5,15 +5,15 @@ set -e
 workdir=$(dirname $(realpath $0))
 version=$(cat version 2>/dev/null)
 bin_name="polaris-server"
-if [ ${GOOS} == "" ];then
+if [ "${GOOS}" == "" ];then
   GOOS=`go env GOOS`
 fi
-if [ ${GOARCH} == "" ];then
+if [ "${GOARCH}" == "" ];then
   GOARCH=`go env GOARCH`
 fi
 folder_name="polaris-server-release_${version}.${GOOS}.${GOARCH}"
 pkg_name="${folder_name}.zip"
-if [ ${GOOS} == "windows" ];then
+if [ "${GOOS}" == "windows" ];then
   bin_name="polaris-server.exe"
 fi
 echo "GOOS is ${GOOS}, binary name is ${bin_name}"
