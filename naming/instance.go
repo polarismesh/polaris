a7378cf4f4fb4733d49fa363b846cbaf14137eef
chuntaojun <liaochuntao@live.com>
2021-12-29 13:57:01 +0800

    revert: revert some code

--- naming/instance.go
+++ naming/instance.go
@@ -555,7 +555,7 @@ func (s *Server) sendDiscoverEvent(eventType model.DiscoverEventType, namespace,
 		EType:     eventType,
 	}
 
-	s.publishDiscoverEvent(event)
+	s.PublishDiscoverEvent(event)
 }
 
 /**
