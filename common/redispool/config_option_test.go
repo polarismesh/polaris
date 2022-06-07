ee5381cfe50155276cb977460a94120d3bd2d6cf
daheige <zhuwei313@hotmail.com>
2022-06-07 21:08:01 +0800

    feat: redispool client test

---
+++ common/redispool/config_option_test.go
@@ -0,0 +1,24 @@
+package redispool
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestNewRedisClient(t *testing.T) {
+	config := DefaultConfig()
+
+	// mock config read
+	config.KvAddr = "127.0.0.1:6379"
+	config.MaxConnAge = 1000
+	config.MinIdleConns = 30
+
+	client := NewRedisClient(WithConfig(config))
+	err := client.Set(context.Background(), "polaris", 1, 60*time.Second).Err()
+	if err != nil {
+		t.Fatalf("new redis client error:%v", err)
+	}
+
+	t.Log("test success")
+}
