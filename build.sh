4043673d7b5a0b7a662463c9c1f713c295e3ac73
赵贵阳 <zhaoguiyang618@gmail.com>
2022-08-02 00:29:05 +0800

    Issue fix/server i18n (#550)
    * finish the hidden special service for console
    
    --issue=459
    
    * fix hidden private method
    
    --issue=459
    
    * add ut
    
    --issue=459
    
    * fix code style
    
    --issue=459
    
    * fix code style
    
    --issue=459
    
    * fix code variable name
    
    --issue=459
    
    * fix code struct
    
    --issue=459
    
    * fix code style
    
    --issue=459
    
    * fix code getServicesFromCacheByName
    
    --issue=459
    
    * add new plugin for main test
    
    --issue=459
    
    * fix default config
    
    --issue=459
    
    * fix config style
    
    --issue=459
    
    * remove hide service plugin
    
    --issue=459
    
    * fix codestyle
    
    --issue=459
    
    * make hiddenlist to hiddenset
    
    --issue=459
    
    * fix code comment
    
    --issue=459
    
    * fix polairs system may not config
    
    --issue=459
    
    * fix code struct
    
    --issue=459
    
    * fix code
    
    --issue=459
    
    * finish i18n
    
    --issue=525
    
    * add  ut
    
    --issue=525
    
    * fix i18n msg
    
    --issue=525
    
    * fix conflict
    
    --issue=525
    
    * add ut
    
    --issue=525
    
    * add ut
    
    --issue=525
    
    * add ut log
    
    --issue=525
    
    * fix integration testing
    
    --issue=525
    
    * fix go mod
    
    --issue=525
    
    * add comment
    
    --issue=525
    
    * fix test comment
    
    --issue=525
    
    Co-authored-by: guiyangzhao <guiyangzhao@tencent.com>

--- build.sh
+++ build.sh
@@ -48,6 +48,7 @@ export CGO_ENABLED=0
 
 build_date=$(date "+%Y%m%d.%H%M%S")
 package="github.com/polarismesh/polaris-server/common/version"
+i18n_res="apiserver/httpserver/i18n"
 go build -o ${bin_name} -ldflags="-X ${package}.Version=${version} -X ${package}.BuildDate=${build_date}"
 
 # 打包
@@ -55,5 +56,7 @@ mkdir -p ${folder_name}
 cp ${bin_name} ${folder_name}
 cp polaris-server.yaml ${folder_name}
 cp -r tool ${folder_name}/
+mkdir -p ${folder_name}/${i18n_res}
+cp -r ${i18n_res}/*.toml ${folder_name}/${i18n_res}
 zip -r "${pkg_name}" ${folder_name}
 md5sum ${pkg_name} >"${pkg_name}.md5sum"
