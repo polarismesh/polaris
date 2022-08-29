d406ad747835f6c59e26f3b1caf466ff4035409d
andrewshan <andrewshan@tencent.com>
2022-08-29 17:30:22 +0800

    feat：arm64编译有问题，暂时先去掉

--- build_docker.sh
+++ build_docker.sh
@@ -11,7 +11,8 @@ docker_repository="polarismesh"
 
 echo "docker repository : ${docker_repository}/polaris-server, tag : ${docker_tag}"
 
-arch_list=( "amd64" "arm64" )
+#arch_list=( "amd64" "arm64" )
+arch_list=( "amd64" )
 platforms=""
 
 for arch in ${arch_list[@]}; do
