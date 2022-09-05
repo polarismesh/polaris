833ffd349ab0d8049b92635b47cd20f7e2c50017
liaochuntao <liaochuntao@live.com>
2022-09-05 15:32:55 +0800

    Release v1.11.0 (#626)
    * feat: 合入增量的限流功能修改 (#567)
    
    * feat: 返回所有字段，避免前端出现问题 (#563)
    
    * fix：官网文档页面错误
    
    * feat: 限流规则优化调整
    
    * fix: invalid default value for etime
    
    * fix: 单机版用例失败问题
    
    * fix：部分单测用例依赖了无效字段
    
    * feat: 修复鉴权用例失败问题
    
    * fix：修复用例失败
    
    * fix: 修复限流规则过滤条件的问题
    
    * fix: 修复用例失败问题
    
    * 修复codecov的单测问题
    
    * fix：调整激活时间的实现，使用真实时间戳
    
    * feat: 修复数据库时间戳问题
    
    * feat: 增加feat的覆盖测试
    
    * feat：INCLUDE改成IN，贴近集合匹配的语义
    
    * feat：接口字段为空的时候不返回 & 补齐action字段
    
    * feat：增加启用限流规则接口
    
    * feat:增加最大排队时长
    
    * feat：提交新变化接口的mock
    
    * feat：返回全部字段，否则前端无法适配
    
    * feat：返回所有的字段，否则前端适配会出问题
    
    * feat: 支持限流规则的最大排队时间字段 (#564)
    
    * fix：官网文档页面错误
    
    * feat: 限流规则优化调整
    
    * fix: invalid default value for etime
    
    * fix: 单机版用例失败问题
    
    * fix：部分单测用例依赖了无效字段
    
    * feat: 修复鉴权用例失败问题
    
    * fix：修复用例失败
    
    * fix: 修复限流规则过滤条件的问题
    
    * fix: 修复用例失败问题
    
    * 修复codecov的单测问题
    
    * fix：调整激活时间的实现，使用真实时间戳
    
    * feat: 修复数据库时间戳问题
    
    * feat: 增加feat的覆盖测试
    
    * feat：INCLUDE改成IN，贴近集合匹配的语义
    
    * feat：接口字段为空的时候不返回 & 补齐action字段
    
    * feat：增加启用限流规则接口
    
    * feat:增加最大排队时长
    
    * feat：提交新变化接口的mock
    
    * feat：返回全部字段，否则前端无法适配
    
    * feat：返回所有的字段，否则前端适配会出问题
    
    * feat：增加MaxQueueDelay值的设置及返回
    
    * feat:支持修改实例时修改地理位置信息 (#578)
    
    * Update docker.yml
    
    * fix: fix docker prometheus (#582)
    
    * Fix delete service check ratelimit (#587)
    
    * fix: fix docker prometheus
    
    * fix: 修复删除服务时无法检查到关联的限流规则
    
    * feat:支持注册polaris.discover以及polaris.healthcheck
    
    * fix: 修复单机版本无法自注册问题
    
    * test: 修复单元测试
    
    * fix: fix standalone install can't modify port
    
    Co-authored-by: andrew shan <45474304+andrewshan@users.noreply.github.com>
    Co-authored-by: heige <zhuwei313@hotmail.com>

--- service/instance.go
+++ service/instance.go
@@ -552,10 +552,16 @@ func (s *Server) updateInstanceAttribute(req *api.Instance, instance *model.Inst
 		insProto.Metadata = req.GetMetadata()
 		needUpdate = true
 	}
+
 	if ok := instanceLocationNeedUpdate(req.GetLocation(), instance.Proto.GetLocation()); ok {
 		insProto.Location = req.Location
 		needUpdate = true
 	}
+
+	// if !needUpdate {
+	// 	// 不需要更新metadata，则置空
+	// 	insProto.Metadata = nil
+	// }
 
 	if req.GetProtocol() != nil && req.GetProtocol().GetValue() != instance.Protocol() {
 		insProto.Protocol = req.GetProtocol()
@@ -1082,6 +1088,21 @@ func preGetInstances(query map[string]string) (map[string]string, map[string]str
 	}
 
 	return filters, metaFilter, nil
+}
+
+func instanceLocationNeedUpdate(req *api.Location, old *api.Location) bool {
+
+	if req.GetRegion().GetValue() != old.GetRegion().GetValue() {
+		return true
+	}
+	if req.GetZone().GetValue() != old.GetZone().GetValue() {
+		return true
+	}
+	if req.GetCampus().GetValue() != old.GetCampus().GetValue() {
+		return true
+	}
+
+	return false
 }
 
 // instance metadata need update
