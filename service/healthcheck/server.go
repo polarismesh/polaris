7160d425c7a20f1acb7372a47e7e99c55b181cf8
chuntaojun <liaochuntao@live.com>
2022-09-17 11:38:57 +0800

    unit:添加单元测试

--- service/healthcheck/server.go
+++ service/healthcheck/server.go
@@ -121,11 +121,6 @@ func initialize(ctx context.Context, hcOpt *Config, cacheOpen bool, bc *batch.Co
 
 // Report heartbeat request
 func (s *Server) Report(ctx context.Context, req *api.Instance) *api.Response {
-	ins := s.instanceCache.GetInstance(req.GetId().GetValue())
-	if ins == nil {
-		return api.NewResponse(api.NotFoundResource)
-	}
-
 	return s.doReport(ctx, req)
 }
 
