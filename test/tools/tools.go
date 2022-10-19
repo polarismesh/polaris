82da0eaf5cdc034f14f814988f300efde36d6fe6
chuntaojun <liaochuntao@live.com>
2022-10-19 16:19:36 +0800

    fix:调整license-checker的触发

--- test/tools/tools.go
+++ test/tools/tools.go
@@ -1,6 +1,3 @@
-//go:build tools
-// +build tools
-
 /**
  * Tencent is pleased to support the open source community by making polaris-go available.
  *
@@ -17,6 +14,9 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
+//go:build tools
+// +build tools
 
 package tools
 
