bb24bfe1922d56c9121f9058986dd2a43f65aee0
BenZhang <zhanglei25@tal.com>
2022-11-16 09:33:47 +0800

    支持 eureka 服务大小写不敏感在北极星的存储方式,调整集成测试。 (#809)
    * 调整集成测试。
    
    * package
    
    * package
    
    * 没有instanceId 去除ins相关
    
    * 直接获取不到app
    
    * add sleep
    
    * 调整 sleep位置 避免getapp为空
    
    * 调整 sleep 15s 避免getapp为空

--- test/uplower_test.go
+++ test/uplower_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+// +build integration
+
 /**
  * Tencent is pleased to support the open source community by making Polaris available.
  *
@@ -15,47 +18,45 @@
  * specific language governing permissions and limitations under the License.
  */
 
-package eurekaserver
+package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/ArthurHlt/go-eureka-client/eureka"
 )
 
 // TestEurekaServer_RegisterApplication 测试EurekaServer大小写
 func TestEurekaServer_RegisterApplication(t *testing.T) {
+	var err error
 	client := eureka.NewClient([]string{
 		"http://127.0.0.1:8761/eureka", //From a spring boot based eureka server
 	})
-	instance := eureka.NewInstanceInfo("TEST.COM", "instanceId", "69.172.200.23", 80, 30, false) //Create a new instance to register
+	appId := "testAPP"
+	//err = client.UnregisterInstance(appId, "4d5c2ab8417452dbd62619359e6116d1eec42a52")
+	//if err != nil {
+	//	t.Fatalf("Eureka UnregisterInstance Error:%+v", err)
+	//}
+
+	instance := eureka.NewInstanceInfo("TEST", appId, "1.1.1.1", 80, 30, false) //Create a new instance to register
 	instance.Metadata = &eureka.MetaData{
 		Map: make(map[string]string),
 	}
-	instance.Metadata.Map["foo"] = "bar"                  //add metadata for example
-	err := client.RegisterInstance("ServiceID", instance) // Register new instance in your eureka(s)
+	instance.Metadata.Map["foo"] = "bar" //add metadata for example
+
+	err = client.RegisterInstance(appId, instance) // Register new instance in your eureka(s)
 	if err != nil {
-		t.Log(err)
+		t.Fatal(err)
 	}
+	time.Sleep(15 * time.Second)
 
 	applications, _ := client.GetApplications() // Retrieves all applications from eureka server(s)
 	t.Log(applications)
 
-	application, err := client.GetApplication(instance.App) // retrieve the application "test"
-	t.Log(application)
+	_, err = client.GetApplication(appId)
 	if err != nil {
-		t.Log(err)
-	}
-
-	ins, err := client.GetInstance(instance.App, instance.HostName) // retrieve the instance from "test.com" inside "test"" app
-	t.Log(ins)
-	if err != nil {
-		t.Log(err)
-	}
-
-	err = client.SendHeartbeat(instance.App, instance.HostName) // say to eureka that your app is alive (here you must send heartbeat before 30 sec)
-	if err != nil {
-		t.Log(err)
+		t.Fatalf("Eureka GetApplication Error:%+v", err)
 	}
 
 }
