a7dc96242254aed927a99fca9922b785a4a0d585
andrewshan <andrewshan@tencent.com>
2022-11-28 19:28:00 +0800

    fix：解决自注册时候，服务不存在需要动态创建的问题

--- bootstrap/server.go
+++ bootstrap/server.go
@@ -27,7 +27,7 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/ptypes/wrappers"
-	yaml "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 
 	"github.com/polarismesh/polaris/apiserver"
 	"github.com/polarismesh/polaris/auth"
@@ -426,6 +426,7 @@ func FinishBootstrapOrder(tx store.Transaction) error {
 func genContext() context.Context {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, utils.StringContext("request-id"), fmt.Sprintf("self-%d", time.Now().Nanosecond()))
+	ctx = context.WithValue(ctx, utils.ContextAuthContextKey, &model.AcquireContext{})
 	return ctx
 }
 
@@ -511,10 +512,6 @@ func selfRegister(
 	if err != nil {
 		return err
 	}
-	storage, err := store.GetStore()
-	if err != nil {
-		return err
-	}
 
 	name := boot_config.DefaultPolarisName
 	polarisNamespace := boot_config.DefaultPolarisNamespace
@@ -524,14 +521,6 @@ func selfRegister(
 
 	if polarisService.Namespace != "" {
 		polarisNamespace = polarisService.Namespace
-	}
-
-	svc, err := storage.GetService(name, polarisNamespace)
-	if err != nil {
-		return err
-	}
-	if svc == nil {
-		return fmt.Errorf("self service(%s) in namespace(%s) not found", name, polarisNamespace)
 	}
 
 	metadata := polarisService.Metadata
@@ -547,7 +536,6 @@ func selfRegister(
 		Host:              utils.NewStringValue(host),
 		Port:              utils.NewUInt32Value(port),
 		Protocol:          utils.NewStringValue(protocol),
-		ServiceToken:      utils.NewStringValue(svc.Token),
 		Version:           utils.NewStringValue(version.Get()),
 		EnableHealthCheck: utils.NewBoolValue(true),
 		Isolate:           utils.NewBoolValue(isolated),
