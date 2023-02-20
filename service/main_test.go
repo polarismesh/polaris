e66280144bfcecc1bb1746d47974fa384b92344f
andrew shan <45474304+andrewshan@users.noreply.github.com>
2023-02-20 11:51:17 +0800

    test: 补充健康检查相关的单测 (#933)
    * feat: upgrade specification to 1.1.0 stable release
    
    * feat：适配V1规则的分组名，V1规则分组名=V2规则名.分组名
    
    * fix: boltdb load int value should be int64
    
    * fix：单机版规则过滤标签不生效
    
    * fix：修复静态列数据更新问题以及统配规则新加入缓存不生效问题
    
    * feat：数据库字段修改为下划线
    
    * fix: 解决mysql数据库写入失败问题
    
    * fix: fix golint erros
    
    * feat：合入1.13.x改动的eureka兼容相关的问题
    
    * fix: golint-ci issues
    
    * feat: 客户端健康检查与服务实例健康检查机制分离
    
    * fix: sqldb healthcheck config
    
    * fix: 修复客户端管理时间单位不一致的问题
    
    * feat：支持存量熔断规则的迁移
    
    * feat：补充心跳上报单测

--- service/main_test.go
+++ service/main_test.go
@@ -103,6 +103,7 @@ type TestConfig struct {
 type DiscoverTestSuit struct {
 	cfg                 *TestConfig
 	server              DiscoverServer
+	healthCheckServer   *healthcheck.Server
 	namespaceSvr        namespace.NamespaceOperateServer
 	cancelFlag          bool
 	updateCacheInterval time.Duration
@@ -243,6 +244,7 @@ func (d *DiscoverTestSuit) initialize(opts ...options) error {
 		panic(err)
 	}
 	healthcheck.SetServer(healthCheckServer)
+	d.healthCheckServer = healthCheckServer
 	cacheProvider, err := healthCheckServer.CacheProvider()
 	if err != nil {
 		panic(err)
