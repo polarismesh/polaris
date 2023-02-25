dbfbea81f53ec0b95ec55e8d459710570cd9899c
andrew shan <45474304+andrewshan@users.noreply.github.com>
2023-02-25 09:58:53 +0800

    fix: eureka updateStatus鉴权失败 (#948)

--- service/instance_test.go
+++ service/instance_test.go
@@ -1659,13 +1659,62 @@ func TestInstanceNoNeedUpdate(t *testing.T) {
 		oldHealthCheck := instanceReq.GetHealthCheck()
 		instanceReq.HealthCheck = nil
 		defer func() { instanceReq.HealthCheck = oldHealthCheck }()
-		So(discoverSuit.server.UpdateInstances(discoverSuit.defaultCtx, []*apiservice.Instance{instanceReq}).GetCode().GetValue(), ShouldEqual, api.NoNeedUpdate)
+		So(discoverSuit.server.UpdateInstances(discoverSuit.defaultCtx,
+			[]*apiservice.Instance{instanceReq}).GetCode().GetValue(), ShouldEqual, api.NoNeedUpdate)
 	})
+}
+
+func TestUpdateInstanceField(t *testing.T) {
+	discoverSuit := &DiscoverTestSuit{}
+	if err := discoverSuit.initialize(); err != nil {
+		t.Fatal(err)
+	}
+	defer discoverSuit.Destroy()
+
+	_, serviceResp := discoverSuit.createCommonService(t, 181)
+	defer discoverSuit.cleanServiceName(serviceResp.GetName().GetValue(), serviceResp.GetNamespace().GetValue())
+
+	_, instanceResp := discoverSuit.createCommonInstance(t, serviceResp, 181)
+	defer discoverSuit.cleanInstance(instanceResp.GetId().GetValue())
+	instId := instanceResp.GetId().GetValue()
+	Convey("metadata变更", t, func() {
+		request := &apiservice.Instance{Id: wrapperspb.String(instId)}
+		request.Metadata = map[string]string{}
+		So(discoverSuit.server.UpdateInstance(
+			discoverSuit.defaultCtx, request).GetCode().GetValue(), ShouldEqual, api.ExecuteSuccess)
+
+		request.Metadata = map[string]string{"123": "456", "789": "abc", "135": "246"}
+		So(discoverSuit.server.UpdateInstance(
+			discoverSuit.defaultCtx, request).GetCode().GetValue(), ShouldEqual, api.ExecuteSuccess)
+
+		instance, err := discoverSuit.storage.GetInstance(instId)
+		So(err, ShouldBeNil)
+		So(instance.Proto.Host.GetValue(), ShouldEqual, instanceResp.Host.GetValue())
+	})
+
+	Convey("isolate变更", t, func() {
+		request := &apiservice.Instance{Id: wrapperspb.String(instId)}
+		request.Isolate = wrapperspb.Bool(true)
+		So(discoverSuit.server.UpdateInstance(
+			discoverSuit.defaultCtx, request).GetCode().GetValue(), ShouldEqual, api.ExecuteSuccess)
+		instance, err := discoverSuit.storage.GetInstance(instId)
+		So(err, ShouldBeNil)
+		So(instance.Proto.Isolate.GetValue(), ShouldEqual, true)
+
+		request.Isolate = wrapperspb.Bool(false)
+		So(discoverSuit.server.UpdateInstance(
+			discoverSuit.defaultCtx, request).GetCode().GetValue(), ShouldEqual, api.ExecuteSuccess)
+
+		instance, err = discoverSuit.storage.GetInstance(instId)
+		So(err, ShouldBeNil)
+		So(instance.Proto.Isolate.GetValue(), ShouldEqual, false)
+	})
+
 }
 
 // 实例数据更新测试
 // 部分数据变更，触发更新
-func TestUpdateInstanceFiled(t *testing.T) {
+func TestUpdateInstancesFiled(t *testing.T) {
 
 	discoverSuit := &DiscoverTestSuit{}
 	if err := discoverSuit.initialize(); err != nil {
