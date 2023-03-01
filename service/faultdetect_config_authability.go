46be4d22d5dbce3b495d9581cb67c5dad7696451
chuntaojun <liaochuntao@live.com>
2023-03-01 15:27:06 +0800

    refactor:source code structure

--- service/faultdetect_config_authability.go
+++ service/faultdetect_config_authability.go
@@ -20,9 +20,6 @@ package service
 import (
 	"context"
 
-	api "github.com/polarismesh/polaris/common/api/v1"
-	"github.com/polarismesh/polaris/common/model"
-	"github.com/polarismesh/polaris/common/utils"
 	apifault "github.com/polarismesh/specification/source/go/api/v1/fault_tolerance"
 	apiservice "github.com/polarismesh/specification/source/go/api/v1/service_manage"
 
