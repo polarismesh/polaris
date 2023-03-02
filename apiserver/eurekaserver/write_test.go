057829c8b43f13f25dbd59991de36e303c1ba6a6
andrewshan <andrewshan@tencent.com>
2023-03-02 16:12:36 +0800

    fix: 修复用例失败问题

--- apiserver/eurekaserver/write_test.go
+++ apiserver/eurekaserver/write_test.go
@@ -142,9 +142,10 @@ func TestEurekaServer_renew(t *testing.T) {
 		svr := &EurekaServer{
 			healthCheckServer: eurekaSuit.healthSvr,
 		}
+		instId := utils.NewUUID()
 		var code uint32
 		for i := 0; i < 5; i++ {
-			code = svr.renew(context.Background(), "", utils.NewUUID(), false)
+			code = svr.renew(context.Background(), "", instId, false)
 			time.Sleep(time.Second)
 		}
 		assert.Equalf(t, api.NotFoundResource, code, "code need notfound, actual : %d", code)
