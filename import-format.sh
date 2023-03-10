1e3f28c8356e91c99f10c828c0cc23eb345393e7
chuntaojun <liaochuntao@live.com>
2023-03-10 10:47:22 +0800

    fix:修复单元测试问题

--- import-format.sh
+++ import-format.sh
@@ -33,8 +33,9 @@ mv goimports-reviser ../
 
 cd ../
 
+# 处理 go 代码格式化
+go fmt ./...
+
 find . -name "*.go" -type f | grep -v .pb.go|grep -v test/tools/tools.go | grep -v ./plugin.go |\
 xargs -I {} ./goimports-reviser -rm-unused -format {} -local github.com/polarismesh/specification -project-name github.com/polarismesh/polaris
 
-# 处理 go 代码格式化
-go fmt ./...
