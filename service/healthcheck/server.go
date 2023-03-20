1873ed104a138bee09aec3cac4dfb735a5c11bc2
chuntaojun <liaochuntao@live.com>
2023-03-20 10:45:28 +0800

    fix:修复发布实例变更事件可能导致的panic

--- service/healthcheck/server.go
+++ service/healthcheck/server.go
@@ -25,9 +25,9 @@ import (
 	"sync"
 	"time"
 
+	"github.com/golang/protobuf/proto"
 	apimodel "github.com/polarismesh/specification/source/go/api/v1/model"
 	apiservice "github.com/polarismesh/specification/source/go/api/v1/service_manage"
-	"github.com/golang/protobuf/proto"
 
 	"github.com/polarismesh/polaris/cache"
 	api "github.com/polarismesh/polaris/common/api/v1"
