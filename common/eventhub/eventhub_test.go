17de3cc1dcd26baca87c33153677ee418a5d4ce1
chuntaojun <liaochuntao@live.com>
2023-04-07 02:04:04 +0800

    style:减少魔法变量

--- common/eventhub/eventhub_test.go
+++ common/eventhub/eventhub_test.go
@@ -168,7 +168,7 @@ func TestEventHub_Shutdown(t *testing.T) {
 			wantTopicNum: 0,
 		},
 	}
-	InitEventHub()
+	TestInitEventHub()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			Shutdown()
