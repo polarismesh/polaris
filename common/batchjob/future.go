2f780971eacade5db835e2743161fc0139f1eb22
chuntaojun <liaochuntao@live.com>
2023-04-12 14:49:23 +0800

    feat:support heartbeat without redis in cluster

---
+++ common/batchjob/future.go
@@ -0,0 +1,62 @@
+/**
+ * Tencent is pleased to support the open source community by making Polaris available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package batchjob
+
+import (
+	"context"
+)
+
+type Task interface{}
+
+type Future interface {
+	TaskInfo() Task
+	Done() (interface{}, error)
+	Cancel()
+	Reply(result interface{}, err error)
+}
+
+type future struct {
+	task     Task
+	err      error
+	result   interface{}
+	isCancel int32
+	ctx      context.Context
+	cancel   context.CancelFunc
+}
+
+func (f *future) TaskInfo() Task {
+	return f.task
+}
+
+func (f *future) Done() (interface{}, error) {
+	<-f.ctx.Done()
+	if err := f.ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	return f.result, f.err
+}
+
+func (f *future) Cancel() {
+	f.cancel()
+}
+
+func (f *future) Reply(result interface{}, err error) {
+	f.result = result
+	f.err = err
+}
