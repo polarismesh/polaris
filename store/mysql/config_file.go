2ac81169a4ba0cf808e511497fc1a3318caa9b14
chuntaojun <liaochuntao@live.com>
2023-08-18 10:44:33 +0800

    refactor:配置文件缓存逻辑重构

--- store/mysql/config_file.go
+++ store/mysql/config_file.go
@@ -22,10 +22,11 @@ import (
 	"strings"
 	"time"
 
+	"go.uber.org/zap"
+
 	"github.com/polarismesh/polaris/common/model"
 	"github.com/polarismesh/polaris/common/utils"
 	"github.com/polarismesh/polaris/store"
-	"go.uber.org/zap"
 )
 
 var (
