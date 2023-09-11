75ed857c8736366cc7c73761bff4a57143e16dd8
wtifs <da_tou@126.com>
2023-09-11 15:53:57 +0800

    add runtime stack info to panic log

--- apiserver/grpcserver/base.go
+++ apiserver/grpcserver/base.go
@@ -263,7 +263,9 @@ func (b *BaseGrpcServer) streamInterceptor(srv interface{}, ss grpc.ServerStream
 
 	defer func() {
 		if panicInfo := recover(); panicInfo != nil {
-			b.log.Errorf("panic %+v", panicInfo)
+			var buf [4086]byte
+			n := runtime.Stack(buf[:], false)
+			b.log.Errorf("panic recovered: %v, STACK: %s", panicInfo, buf[0:n])
 		}
 	}()
 
