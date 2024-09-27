b950bb12ef6187a4d8a57cd51c4cc47694467dc9
chuntaojun <liaochuntao@live.com>
2024-09-27 09:53:00 +0800

    refactor:鉴权能力优化调整

--- store/mysql/role.go
+++ store/mysql/role.go
@@ -267,7 +267,8 @@ func (s *roleStore) GetMoreRoles(firstUpdate bool, mtime time.Time) ([]*authcomm
 }
 
 func (s *roleStore) fetchRolePrincipals(tx *BaseTx, role *authcommon.Role) error {
-	rows, err := tx.Query("SELECT role_id, principal_id, principal_role, IFNULL(extend_info, '') FROM auth_role_principal WHERE rold_id = ?", role.ID)
+	rows, err := tx.Query("SELECT role_id, principal_id, principal_role, IFNULL(extend_info, '') FROM "+
+		" auth_role_principal WHERE rold_id = ?", role.ID)
 	if err != nil {
 		log.Error("[store][role] fetch role principals", zap.String("name", role.Name), zap.Error(err))
 		return store.Error(err)
